8748f6cbb73daf5ead2746b400c49d49fc9174de
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-11-29 22:47:10 +0500

    main file updated

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from modules import modules_router
+
+app = FastAPI()
+
+
+app.include_router(
+    modules_router,
+    prefix='/kernel-svm-rbf',
+    tags=['Kernel SVM RBF']
+)
